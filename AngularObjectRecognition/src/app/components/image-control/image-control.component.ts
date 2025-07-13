29129204db251426b89637283becfdf103a4dbc8
afilinko <01194269@pw.edu.pl>
2025-07-14 00:23:48 +0200

    autocreation of angular app

---
+++ AngularObjectRecognition/src/app/components/image-control/image-control.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-image-control',
+  standalone: true,
+  imports: [],
+  templateUrl: './image-control.component.html',
+  styleUrl: './image-control.component.css'
+})
+export class ImageControlComponent {
+
+}
