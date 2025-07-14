c5ba002710e8814aa04563f0d7329527de2f430e
afilinko <01194269@pw.edu.pl>
2025-07-14 13:14:16 +0200

    created an application template

--- AngularObjectRecognition/src/app/app.component.ts
+++ AngularObjectRecognition/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ImageControlComponent } from './components/image-control/image-control.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ImageControlComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- AngularObjectRecognition/src/app/components/image-control/image-control.component.ts
+++ AngularObjectRecognition/src/app/components/image-control/image-control.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-image-control',
@@ -8,5 +9,9 @@ import { Component } from '@angular/core';
   styleUrl: './image-control.component.css'
 })
 export class ImageControlComponent {
-
+  imageUrl: string | null = null;
+  selectedFile: File | null = null;
+  // prediction$: Observable<any[] | null>;
+  // loading$: Observable<boolean>;
+  // error$: Observable<string | null>;
 }
