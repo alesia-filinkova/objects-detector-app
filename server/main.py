a8b80eaf410f76f3400a4f3232bbfdb4c6d84d6a
afilinko <01194269@pw.edu.pl>
2025-07-23 17:33:36 +0300

    code to identify objects

--- server/main.py
+++ server/main.py
@@ -1,22 +1,54 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
+from typing import Dict, Any
+from label_map import label_map
+
+import tensorflow as tf
+import numpy as np
+import cv2
+import os
+
+os.environ["TF_ENABLE_ONEDNN_OPTS"] = '0'
+
+MODEL_DIR = "ssd_mobilenet_v2_coco/saved_model"
+model = tf.saved_model.load(str(MODEL_DIR))
+infer = model.signatures['serving_default']
+
+def run_inference(image: np.ndarray) -> Dict[str, Any]:
+    input_tensor = tf.convert_to_tensor(image)
+    input_tensor = input_tensor[tf.newaxis, ...]
+    
+    detections = infer(input_tensor)
+    
+    return detections
 
 app = FastAPI()
+
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)) -> JSONResponse:
+    contents = await file.read()
+    image = np.array(cv2.imdecode(np.frombuffer(contents, np.uint8), cv2.IMREAD_COLOR))
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    detections = run_inference(image_rgb)
+    num_detections = int(detections.pop('num_detections'))
+    detections = {key: value[0, :num_detections].numpy() for key, value in detections.items()}
+    
+    for key in detections:
+        detections[key] = detections[key].tolist()
+    
+    results = []
+    
+    for i in range(num_detections):
+        if detections['detection_scores'][i] >= 0.5:
+            class_id = int(detections["detection_classes"][i]) 
+            category = label_map.get(class_id, 'unknown')
+            box = detections['detection_boxes'][i]
+            score = detections["detection_scores"][i]
+            results.append({'box': box, 'category': category, 'scores': score})
+    
+    print("Final ", results)
+    return JSONResponse(content=results)
 
 @app.get("/")
 async def root():
     return {"message": "Hello world!"}
-
-items = {
-    1: {"name": "Item one", "description": "This is the first item"},
-    2: {"name": "Item two", "description": "This is the second item"},
-    3: {"name": "Item three", "description": "This is the third item"},
-}
-
-@app.get("/items/{item_id}")
-def get_item(item_id: int):
-    item = items.get(item_id)
-    
-    if item is None:
-        return {"error": "Item not found"}, 404
-    
-    return item
