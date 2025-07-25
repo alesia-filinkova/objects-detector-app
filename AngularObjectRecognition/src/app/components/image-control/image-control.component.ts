8ada38a8c309298efa1f3058649b0c6814412fb6
afilinko <01194269@pw.edu.pl>
2025-07-25 15:21:42 +0300

    integrate image upload logic and prediction observables

--- AngularObjectRecognition/src/app/components/image-control/image-control.component.ts
+++ AngularObjectRecognition/src/app/components/image-control/image-control.component.ts
@@ -1,17 +1,51 @@
+import { AsyncPipe, UpperCasePipe, PercentPipe } from '@angular/common';
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { Prediction } from '../../types/types';
+import { UploadService } from '../../services/upload.service';
+import { enableDebugTools } from '@angular/platform-browser';
+import { api_key } from '../../keys/api_keys';
 
 @Component({
   selector: 'app-image-control',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe, UpperCasePipe, PercentPipe],
   templateUrl: './image-control.component.html',
   styleUrl: './image-control.component.css'
 })
 export class ImageControlComponent {
   imageUrl: string | null = null;
   selectedFile: File | null = null;
-  // prediction$: Observable<any[] | null>;
-  // loading$: Observable<boolean>;
-  // error$: Observable<string | null>;
+  prediction$: Observable<Prediction[] | null>;
+  loading$: Observable<boolean>;
+  error$: Observable<string | null>;
+
+  constructor(private uploadImageService: UploadService) {
+    this.prediction$ = this.uploadImageService.prediction$
+    this.loading$ = this.uploadImageService.loading$
+    this.error$ = this.uploadImageService.error$
+  }
+
+  onFileSelected(event: Event) : void {
+    const element = event.currentTarget as HTMLInputElement;
+    const file = element.files?.[0]
+    if (file) {
+      this.selectedFile = file;
+      this.imageUrl = URL.createObjectURL(file)
+    }
+  }
+
+  uploadImage() : void {
+    if (this.selectedFile) {
+      of(
+        this.uploadImageService.uploadImage(this.selectedFile, `${api_key}/predict`)
+      ).subscribe({
+        next: (v) => (this.prediction$ = v),
+        error: (error) => (this.error$ = error),
+        complete: () => console.info('complete')
+      });
+    } else {
+      console.error('No File Selected')
+    }
+  }
 }
